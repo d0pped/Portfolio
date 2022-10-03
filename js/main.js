f238e11c04bc3a4eaaebef7012f2942bb4585e51
d0pped <zuh0301@gmail.com>
2022-10-04 04:51:11 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+    const progressBox = $(".progress-bar")
+    const progress0st = $(".animation").offset().top - 600;
+    let isAni=false;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= progress0st && !isAni ) {
+            progressAni();
+        }
+    })
+function progressAni(){
+        progressBox.each(function () {
+            let $this = $(this),
+                progressBar = $this.find(".bar"),
+                progressText = $this.find(".rate"),
+                progressRate = progressText.attr("data-rate");
+            progressBar.animate({ width: progressRate + "%" }, 2500);
+
+            let text = function () {
+                $({ rate: 0 }).animate(
+                    { rate: progressRate },
+                    {
+                        duration: 2000,
+                        progress: function () {
+                            let now = this.rate;
+                            progressText.text(Math.floor(now) + '%');
+                        },
+                        complete:function(){
+                            isAni=true
+                        progressBox.addClass()
+                    }
+                    })
+            };
+            text();
+        });
+    }
+});
